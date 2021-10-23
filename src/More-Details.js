9d6f655a6ba1d5f72274563b160852af7c21c623
abuzarkhan <abuzarkhan5468@gmail.com>
2021-10-23 21:30:51 +0500

    title updated

--- src/More-Details.js
+++ src/More-Details.js
@@ -9,9 +9,9 @@ const MoreDetails = ({current}) => {
     return (
         <div className='details'>
             <ResultCard icon={ <GiWindyStripes size='2.8rem'/>} title="Wind" value={current?.wind_speed} unit="mph"/>
-            <ResultCard icon={ <GiWindSlap size='2.8rem'/>} title="Wind" value={current?.pressure} unit="mph"/>
-            <ResultCard icon={ <WiHumidity size='3.5rem'/>} title="Wind" value={current?.humidity} unit="mph"/>
-            <ResultCard icon={ <BsFillCloudsFill size='3.5rem'/>} title="Wind" value={current?.cloudcover} unit="mph"/>
+            <ResultCard icon={ <GiWindSlap size='2.8rem'/>} title="Pressure" value={current?.pressure} unit="mph"/>
+            <ResultCard icon={ <WiHumidity size='3.5rem'/>} title="Humidity" value={current?.humidity} unit="mph"/>
+            <ResultCard icon={ <BsFillCloudsFill size='3.5rem'/>} title="Cloud" value={current?.cloudcover} unit="mph"/>
            
             
         </div>

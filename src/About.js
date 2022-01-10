542e75d4779568d12e4a20acc9fbe91b0a4b0d9d
abuzarkhan <abuzarkhan5468@gmail.com>
2022-01-10 18:11:42 +0500

    commit

--- src/About.js
+++ src/About.js
@@ -6,12 +6,12 @@ const About = () =>{
   
     return (
        <div>
-           <h1 style={{color:'red'}}>City Name: {Pack.data.location.name}</h1>
-           <h2>Current Temp: {Pack.data.current.temperature}</h2>
-           <h2>Wind Speed: {Pack.data.current.wind_speed} </h2>
-           <h2>Wind Pressure: {Pack.data.current.wind_pressure} </h2>
-           <h2>Humidity: {Pack.data.current.humidity} </h2>
-           <h2>Cloud Cover: {Pack.data.current.cloudcover} </h2>
+           <h1 style={{color:'red'}}>City Name: {Pack.data.location?.name}</h1>
+           <h2>Current Temp: {Pack.data.current?.temperature}</h2>
+           <h2>Wind Speed: {Pack.data.current?.wind_speed} </h2>
+           <h2>Wind Pressure: {Pack.data.current?.wind_pressure} </h2>
+           <h2>Humidity: {Pack.data.current?.humidity} </h2>
+           <h2>Cloud Cover: {Pack.data.current?.cloudcover} </h2>
            <p>Go back to see weather app</p>
            <Link to="/weatherapp">Go back</Link>
        </div>
